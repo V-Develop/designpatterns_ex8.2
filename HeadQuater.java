d5720660760f319dd331291ef22e0e06c9ba5d37
Alongkorn Vanzoh <68703454+V-Develop@users.noreply.github.com>
2024-02-13 01:11:11 +0700

    Update HeadQuater.java

--- HeadQuater.java
+++ HeadQuater.java
@@ -2,11 +2,6 @@ public class HeadQuater extends Source {
 
     private String result;
 
-    // public void setResult(String result) {
-    //     this.result = result;
-    //     notifyObservers();
-    // }
-
     public void setResult(String result) {
         this.result = result;
         notifyObservers();
